81a4874e36aa1471c3d31af0d3ada0f4c546e0c1
PriyanLinardi <96510386+PriyanLinardi@users.noreply.github.com>
2022-07-13 10:55:01 +0700

    Add files via upload

---
+++ CarSharing/CarSharingTests/Models/CarGenerateTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarSharing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSharing.Models.Tests
+{
+    [TestClass()]
+    public class CarGenerateTests
+    {
+        [TestMethod()]
+        public void IsAvailableTest()
+        {
+            int x = 3, y = 2;
+            Car car = new Car{ PositionX = 4, PositionY = 2 };
+            CarGenerate carGenerate = new CarGenerate(car);
+            Assert.IsTrue(carGenerate.IsAvailable(x,y));
+        }
+    }
+}
